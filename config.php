9b6779e80dd6c808564395482ab52143d6950566
unaxsys <ogi.stoev80@gmail.com>
2026-02-21 13:24:39 +0200

    1

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('ADMIN_USER', 'admin');
 
 // ✅ СМЕНИ паролата: по подразбиране е "ChangeMe_123!"
 // Ако искаш нова парола, кажи ми и ще ти дам нов hash.
-define('ADMIN_PASS_HASH', '$2y$10$uArtofqvwQ27z.iSyVEJ.O8LPgFeeNofyyPuLI/9MFC0rYuI/4Qx2');
+define('ADMIN_PASS_HASH', '$2y$10$qQ3WtRKA4HIHsNM/m7RCOOPzeKxxedSkxe9a9AktWUidyJbl.ddv.');
 
 // SQLite файл (ще се създаде автоматично)
 define('DB_PATH', __DIR__ . '/db.sqlite');
