a96fa9f54f3ab9addd46eab2615027b528525371
unaxsys <ogi.stoev80@gmail.com>
2026-02-24 09:10:23 +0200

    1

--- includes/config.php
+++ includes/config.php
@@ -9,3 +9,14 @@ if (!defined('MYSQL_USER')) define('MYSQL_USER', 'magosbgf_bloguser');
 if (!defined('MYSQL_PASS')) define('MYSQL_PASS', 'Lamer4e*#');
 
 if (!defined('SITE_URL'))   define('SITE_URL', 'https://magos.bg');
+// ===========================
+// ADMIN LOGIN CONFIG
+// ===========================
+if (!defined('ADMIN_USER')) {
+  define('ADMIN_USER', 'admin');
+}
+
+if (!defined('ADMIN_PASS_HASH')) {
+  // TEMP placeholder – ще го сменим след 2 минути с валиден hash
+  define('$2y$12$PIfrSLFIjTlPoB.iHmflSePdSQRHP1WQdaYJUcbg./SJFSb3i/wOS, '');
+}
